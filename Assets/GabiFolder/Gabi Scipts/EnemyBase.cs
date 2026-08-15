ddaac40ed76d6d2d553b219ec3dcb8517ead03f7
GabiNieto <gabi.nieto00@gmail.com>
2026-08-15 15:51:50 -0500

    workingOnEnemy.PlayerPrefab

--- Assets/GabiFolder/Gabi Scipts/EnemyBase.cs
+++ Assets/GabiFolder/Gabi Scipts/EnemyBase.cs
@@ -2,15 +2,28 @@ using UnityEngine;
 
 public class EnemyBase : MonoBehaviour
 {
+
+    private Collider2D myCollision;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        myCollision = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+
+        }
     }
 }
